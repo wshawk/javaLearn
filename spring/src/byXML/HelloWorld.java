0d6442ac7ca7998cae50bac7c189362776fba66d
wen <1242293215@qq.com>
2019-01-18 15:52:51 +0800

    Create HelloWorld.java

---
+++ spring/src/byXML/HelloWorld.java
@@ -0,0 +1,13 @@
+package byXML;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Hello Spring By XML");
+    }
+}
